1ad850063b3a6215e7a2caf6affd0cb95d5818b5
jonschlinkert <dev@sellside.com>
2014-04-10 16:40:30 -0400

    rename

--- test/prop-strings.js
+++ test/prop-strings.js
@@ -14,24 +14,25 @@ const plasma = require('../');
  * replace `:props` in `name`
  */
 
-describe('plasma.process():', function () {
-  describe('when plasma.process() is used on a config object', function () {
+describe(':when prop strings: are used in the `name` field', function () {
+  describe('if :basename is used', function () {
     it('should use the basename of each data file as the namespace for its config object', function (done) {
       var fixture = {name: ':basename', src: ['test/fixtures/*.{json,yml}']};
       var expected = {a: {aaa: 'bbbb'}, b: {ccc: 'dddd'}, c: {eee: 'ffff'} };
       expect(plasma.load(fixture)).to.deep.equal(expected);
       done();
     });
+
+    it('should use the basename of each data file as the namespace for its config object', function (done) {
+      var fixture = [{name: ':basename', src: ['test/fixtures/bower-pkg/*.json']}];
+      var expected = file.readJSONSync('test/expected/bower-pkg.json');
+      var actual = plasma.process(fixture);
+
+      expect(actual).to.deep.equal(expected);
+      done();
+    });
   });
 
-  it('should use the basename of each data file as the namespace for its config object', function (done) {
-    var fixture = [{name: ':basename', src: ['test/fixtures/bower-pkg/*.json']}];
-    var expected = file.readJSONSync('test/expected/bower-pkg.json');
-    var actual = plasma.process(fixture);
-
-    expect(actual).to.deep.equal(expected);
-    done();
-  });
 
   it('should use the basename of each data file as the namespace for its config object', function (done) {
     var fixture = [{name: ':basename', src: ['test/fixtures/i18n/*.json'] }];
